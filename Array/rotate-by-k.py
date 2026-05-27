0965eca6b4ca9c5abf121c285f42ab23ea948765
mr zero <mohitraghav1318@gmail.com>
2026-05-27 18:26:27 +0530

    Rotate by k elements in an array

--- Array/rotate-by-k.py
+++ Array/rotate-by-k.py
@@ -23,4 +23,49 @@ leftRotate(arr,3)
 rightRotate(arr,2)       
 '''
 
-#2nd Approach 
+#2nd Approach  by using reverse algorithm
+
+import dataclasses
+def reverse(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+
+
+def leftRotate(arr, d):
+    n = len(arr)
+
+    d = d % n
+
+    reverse(arr, 0, d - 1)
+    reverse(arr, d, n - 1)
+    reverse(arr, 0, n - 1)
+
+    print("Left Rotate :", arr)
+
+
+def rightRotate(arr, d):
+    n = len(arr)
+
+    d = d % n
+
+    d = n - d
+
+    reverse(arr, 0, d - 1)
+    reverse(arr, d, n - 1)
+    reverse(arr, 0, n - 1)
+
+    print("Right Rotate :", arr)
+
+
+arr1 = [1, 3, 5, 4, 5, 3, 2, 7, 8]
+
+print("Original Array :", arr1)     
+
+print("enter no of times")
+d= int(input())
+
+leftRotate(arr1, d)
+
+rightRotate(arr1, d)   
